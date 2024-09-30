34a5f4342922fdd2c8e033cecf960d80fbebaa57
JakeMackBrown <JakeMackBrown@gmail.com>
2024-09-30 18:04:43 -0500

    Connect-Four

--- connect4.js
+++ connect4.js
@@ -1,10 +1,44 @@
-const playerOne = "B";
-const playerTwo = "R";
+//const statusDisplay = document.querySelector('.game--status');
+
+let playerOne = "B";
+let playerTwo = "R";
 const currentPlayer = playerOne;
 
-const gameOver = false;
+let gameActive = true;
+let board;
 
 const rows = 6;
 const columns = 7;
 
-window.onload = function
+//document.querySelector('.game--restart').addEventListener('click', startGame);
+
+window.onload = function () {
+    startGame()
+}
+
+function startGame () {
+    board = [];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+
+            row.push(' ')
+
+            let tile = document.createElement('div');
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById('board').append(tile);
+        }
+        board.push(row);
+    }
+} 
+
+// function startGame() {
+//     gameActive = true;
+//     currentPlayer = "B";
+//     gameState = ["", "", "", "", "", "", "", "", ""];
+//     statusDisplay.innerHTML = currentPlayerTurn();
+//     document.querySelectorAll('.cell')
+//                 .forEach(cell => cell.innerHTML = "")
+// }
